76054663662731c21b662ae7855e8d913c605adb
kenyafan <kenyafan321@gmail.com>
2024-03-24 23:50:53 +0200

    up

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import userData from "./components/Profile/userData.json";
+import { Profile } from "./components/Profile/Profile";
+
+const App = () => {
+  return (
+    <>
+      <Profile
+        name={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        image={userData.avatar}
+        stats={userData.stats}
+      />
+    </>
+  );
+};
